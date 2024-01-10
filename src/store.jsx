3744f609c5f83384b1f796915cf101c955925583
Annath Beevi <annathbeevi01@gmail.com>
2024-01-10 19:03:52 +0530

    Redux persist

--- src/store.jsx
+++ src/store.jsx
@@ -1,14 +1,27 @@
 import accountReducer from "./Components/Features/Account/accountSlice"
 import { customerReducer } from "./Components/Features/Customers/customerSlice"
 import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
 import { configureStore } from '@reduxjs/toolkit'
 
+const persistConfig = {
+    key: "root",
+    storage,
+}
+
+const PersistedAccountReducer = persistReducer(persistConfig, accountReducer)
+
+const PersistedCustomerReducer = persistReducer(persistConfig, customerReducer)
+
 const store = configureStore({
     reducer: {
-        account: accountReducer,
-        customer: customerReducer,
+        account: PersistedAccountReducer,
+        customer: PersistedCustomerReducer,
     }
 })
 
-export default store
+const persistor = persistStore(store)
+
+
+export { store, persistor}
